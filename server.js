6397ca7940acf696c3f69fb2f1c61e39c39445a3
carrieTseng <echocarriet@gmail.com>
2023-05-28 18:46:12 +0800

    調整 POST

--- server.js
+++ server.js
@@ -117,4 +117,4 @@ const requestListener = (req, res) => {
 
 }
 const server = http.createServer(requestListener);
-server.listen(3005); // 瀏覽器使用 127.0.0.1:3005開啟
+server.listen(process.env.PORT || 3005); // 瀏覽器使用 127.0.0.1:3005開啟
